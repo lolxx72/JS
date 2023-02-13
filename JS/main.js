a9edd410dc9ec87363e31af472129a2af297ee4c
lolxx72 <ingridheis11@gmail.com>
2023-02-13 01:59:51 -0300

    actualizacion

--- JS/main.js
+++ JS/main.js
@@ -1,155 +1,153 @@
 const Productos = [
-  {
-    id: 1,
-    nombre: "Lapices",
-    precio: 100,
-  },
-  {
-    id: 2,
-    nombre: "Cuadernillos",
-    precio: 250,
-  },
-  {
-    id: 3,
-    nombre: "Lapiceras",
-    precio: 150,
-  },
-  {
-    id: 4,
-    nombre: "Mochilas lisas",
-    precio: 8000,
-  },
-  {
-    id: 5,
-    nombre: "Mochilas diseño variado",
-    precio: 25000,
-  },
-];
+    {id: 1, nombre: "Lapices clásicos", tipo: "libreria", cantidad: 1, desc: "Lapiz clasico", precio: 100, img: './img/lapiz.png'},
+    {id: 2, nombre: "Lapices de colores", tipo: "libreria", cantidad: 1, desc: "Lapices de colores x12u", precio: 2500, img: './img/colores.jpg'},
+    {id: 3, nombre: "Fibras de colores", tipo: "librería", cantidad: 1, desc: "Fibras de colores x6u", precio: 2000, img: './img/fibras.jpg'},
+    {id: 4, nombre: "Cuaderno Rivadavia", tipo: "cuadernos", cantidad: 1, desc: "Cuaderno Rivadavia hojas rayadas", precio: 560, img: './img/rivadavia.jpg'},
+    {id: 5, nombre: "Cuaderno Exito", tipo: "cuadernos", cantidad: 1, desc: "Cuaderno Exito hojas rayadas", precio: 800, img: './img/exito.jpg'},
+    {id: 6, nombre: "Cuadernillo hoja rayada", tipo: "cuadernillos", cantidad: 1, desc: "Cuadernillo hojas rayadas", precio: 1000, img: './img/cuadernillos.jpg'},
+    {id: 7, nombre: "Cuadernillo hoja a cuadros", tipo: "cuadernillos", cantidad: 1, desc: "Cuadernillo hojas a cuadros", precio: 1000, img: './img/cuadernillos.jpg'},
+    {id: 8, nombre: "Carpeta diseño variado", tipo: "carpetas", cantidad: 1, desc: "Carpetas diseño random", precio: 500, img: './img/carpetas.jpg'},
+    {id: 9, nombre: "Calculadora cientifica Casio", tipo: "variado", cantidad: 1, desc: "Calculadora cientifica Casio", precio: 4500, img: './img/casio.jpg'},
+    {id: 10, nombre: "Calculadora", tipo: "variado", cantidad: 1, desc: "Calculadora clásica", precio: 700, img: './img/calcu.jpg'},
+    {id: 11, nombre: "Set de fibras Sharpie", tipo: "libreria", cantidad: 1, desc: "Set de fibras Sharpie x16", precio: 6000, img: './img/sharpie.jpg'},
+    {id: 12, nombre: "Kit League of Legends (Carpeta + Cartuchera)", tipo: "variado", cantidad: 1, desc: "Carpeta y cartu personalizadas de LoL", precio: 3500, img: './img/lol.jpg'},
+    {id: 13, nombre: "Agendas 2023", tipo: "variado", cantidad: 1, desc: "Agendas 2023", precio: 850, img: './img/agendas.jpg'},
+    {id: 14, nombre: "Libro de notas", tipo: "cuadernillos", cantidad: 1, desc: "Cuaderno para notas", precio: 1400, img: './img/notas.png'},
+    {id: 15, nombre: "Gomas para borrar", tipo: "libreria", cantidad: 1, desc: "Gomas", precio: 70, img: './img/gomas.jpg'},
+    {id: 16, nombre: "Mochila lisa", tipo: "variado", cantidad: 1, desc: "Mochila lisa negra", precio: 6500, img: './img/mochilisa.jpg'},
+    {id: 17, nombre: "Mochila animada", tipo: "variado", cantidad: 1, desc: "Mochila con animación variada", precio: 27500, img: './img/mochidiseño.png'},
+    {id: 18, nombre: "Mochila diseñada", tipo: "variado", cantidad: 1, desc: "Mochila con diseño variado", precio: 10250, img: './img/mochianimada.jpg'},
+]
 
-let Carrito = [];
+const contenedorProductos = document.getElementById('contenedor-productos')
 
-const divisa = "$";
+const contenedorCarrito = document.getElementById('carrito-contenedor')
 
-const carritoItems = document.querySelector("#items");
+const botonVaciar = document.getElementById('vaciar-carrito')
 
-const carritoHTML = document.querySelector("#carrito");
+const contadorCarrito = document.getElementById('contadorCarrito')
 
-const totalHTML = document.querySelector("#total");
+const cantidad = document.getElementById('cantidad')
 
-const botonVaciar = document.querySelector("#boton-vaciar");
+const precioTotal = document.getElementById('precioTotal')
 
-function renderizarProductos() {
-  Productos.forEach((info) => {
-    const contenedor = document.createElement("div");
-    contenedor.classList.add("card", "col-sm-4");
+const cantidadTotal = document.getElementById('cantidadTotal')
 
-    const CardBody = document.createElement("div");
-    CardBody.classList.add("card-body");
+const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
 
-    const miTitle = document.createElement("h5");
-    miTitle.classList.add("card-title");
-    miTitle.textContent = info.nombre;
+const botonAbrir = document.getElementById('boton-carrito')
 
-    const miImagen = document.createElement("img");
-    miImagen.classList.add("img-fluid");
-    miImagen.setAttribute("src", info.imagen);
+const botonCerrar = document.getElementById('carritoCerrar')
 
-    const miProdPrecio = document.createElement("p");
-    miProdPrecio.classList.add("card-text");
-    miProdPrecio.textContent = `${info.precio}${divisa}`;
+const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
 
-    const miBoton = document.createElement("button");
-    miBoton.classList.add("btn", "btn-primary");
-    miBoton.textContent = "Agregar";
-    miBoton.setAttribute("marcador", info.id);
-    miBoton.addEventListener("click", anyadirProductoAlCarrito);
+let carrito = []
 
-    CardBody.append(miImagen, miTitle, miProdPrecio, miBoton);
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
 
-    contenedor.append(CardBody);
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
 
-    carritoItems.append(contenedor);
-  });
-}
+    actualizarCarrito()
+})
 
-function anyadirProductoAlCarrito(evento) {
-  Carrito.push(evento.target.getAttribute("marcador"));
+Productos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img} alt= "">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.desc}</p>
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
 
-  renderizarCarrito();
-}
+    `
+    contenedorProductos.appendChild(div)
 
-function renderizarCarrito() {
-  carritoHTML.textContent = "";
+    const boton = document.getElementById(`agregar${producto.id}`)
 
-  const carritoSinDuplicados = [new Set(Carrito)];
-  carritoSinDuplicados.forEach((item) => {
+
+    boton.addEventListener('click', () => {
+        agregarAlCarrito(producto.id)
+    })
+})
+
+const agregarAlCarrito = (prodId) => {
+
+    const existe = carrito.some (prod => prod.id === prodId) 
+
+    if (existe){ 
+        const prod = carrito.map (prod => { 
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    } else { 
+        const item = Productos.find((prod) => prod.id === prodId)
+
+        carrito.push(item)
+    }
     
-    const miItem = Productos.filter((itemProductos) => {
-      return itemProductos.id === parseInt(item);
-    });
-
-    const numeroUnidadesItem = Carrito.reduce((total, itemId) => {
-      return itemId === item ? (total += 1) : total;
-    }, 0);
-
-    const miCarrito = document.createElement("li");
-    miCarrito.classList.add("list-group-item", "text-right", "mx-2");
-    miCarrito.textContent = `${numeroUnidadesItem} x ${miItem.nombre} - ${miItem.precio}${divisa}`;
-
-    const miBoton = document.createElement("button");
-    miBoton.classList.add("btn", "btn-danger", "mx-5");
-    miBoton.textContent = "Eliminar Producto";
-    miBoton.style.marginLeft = "1rem";
-    miBoton.dataset.item = item;
-    miBoton.addEventListener("click", borrarItemCarrito);
-
-    miCarrito.append(miBoton);
-    carritoHTML.append(miCarrito);
-  });
-  totalHTML.textContent = calcularTotal();
+    actualizarCarrito() 
 }
 
-function borrarItemCarrito(evento) {
-  const id = evento.target.dataset.item;
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
 
-  Carrito = Carrito.filter((carritoId) => {
-    return carritoId !== id;
-  });
+    const indice = carrito.indexOf(item) 
 
-  renderizarCarrito();
+    carrito.splice(indice, 1) 
+
+    actualizarCarrito() 
+    console.log(carrito)
 }
 
-function vaciarCarrito() {
-  Carrito = [];
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = "" 
+    
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
 
-  renderizarCarrito();
+        contenedorCarrito.appendChild(div)
+        
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+
+    })
+    
+    contadorCarrito.innerText = carrito.length 
+
+    console.log(carrito)
+
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+    
+
 }
 
-botonVaciar.addEventListener("click", vaciarCarrito);
+botonAbrir.addEventListener('click', ()=>{
+    contenedorModal.classList.toggle('modal-active')
+})
+botonCerrar.addEventListener('click', ()=>{
+    contenedorModal.classList.toggle('modal-active')
+})
 
-function calcularTotal() {
-  return Carrito.reduce((total, item) => {
-    const miItemTotal = Productos.filter((itemProd) => {
-      return itemProd.id === parseInt(item);
-    });
+contenedorModal.addEventListener('click', (event) =>{
+    contenedorModal.classList.toggle('modal-active')
 
-    return total + miItemTotal[0].precio;
-  }, 0).toFixed(2);
-}
+})
+modalCarrito.addEventListener('click', (event) => {
+    event.stopPropagation() 
+})
 
 
-function leerCarritoEnLS () {
-
-  let devolverCarrito = [];
-
-  const carritoLS = localStorage.getItem("Carrito");
-
-  if(carritoLS !== null) {
-      devolverCarrito = JSON.parse(carritoLS);
-  }
-
-  return devolverCarrito;
-}
-
-renderizarProductos();
-renderizarCarrito();
